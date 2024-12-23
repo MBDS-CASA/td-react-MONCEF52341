352d49fe2323dcb467ad7ba1af18003ef805bb56
MONCEF52341 <mostaine.moncef177@gmail.com>
2024-12-23 16:13:14 +0100

    TD03 : Afficher les détails d'une note

--- td-react/src/components/GradeInfo.jsx
+++ td-react/src/components/GradeInfo.jsx
@@ -4,7 +4,7 @@ const GradeInfo = ({ gradeInfo }) => {
   const { unique_id, course, student, date, grade } = gradeInfo;
 
   return (
-    <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div className="bg-green-400 shadow-md rounded px-8 pt-6 pb-8 mb-4">
       <h2 className="text-xl font-bold mb-4">{course}</h2>
       <p><strong>Student:</strong> {student.firstname} {student.lastname} (ID: {student.id})</p>
       <p><strong>Date:</strong> {date}</p>
